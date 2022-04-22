b6d8bd06d32f6e42a21d5ea1ae161aa722636089
97016086 Renate Pit <97016086@st.deltion.nl>
2022-04-22 09:10:39 +0200

    de functie reset aangemaakt en de steenpapierschaar.html aangemaakt.

--- app.js
+++ app.js
@@ -2,18 +2,33 @@ const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
-let computerPoints = document.getElementById('computer-points');
-let userPoints = document.getElementById('user-points');
+const ronde = document.getElementById('ronde');
+const winner = document.getElementById('winner');
+const computerPoints = document.getElementById('computer-points');
+const userPoints = document.getElementById('user-points');
 let userChoice;
 let computerChoice;
+let computerpunten = 0;
+let gebruikerpunten = 0;
 let result;
+let round = 0;
+let winnaar;
 
+
+//de e staat voor event en gaat als functie door de click functie. 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    if (round === 5){
+        return;
+    }
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    round++;
     generateComputerChoice();
     getResult();
+    uiteindelijkeWinnaar();
 
+    ronde.innerHTML = round;
+    reset();
 }));
 
 function generateComputerChoice() {
@@ -32,8 +47,7 @@ function generateComputerChoice() {
 }
 
 function getResult() {
-    computerPoints = 0;
-    userPoints = 0;
+
 
     if (computerChoice === userChoice){
         result = 'Het is een Gelijkspel';
@@ -41,25 +55,67 @@ function getResult() {
     }
 
     if(computerChoice =='steen' && userChoice == 'papier'){
-       result = 'Jij hebt gewonnen';
-       userPoints++;
+       result = 'Jij hebt deze ronde gewonnen';
+       ++gebruikerpunten;
     }else if(computerChoice == 'papier' && userChoice == 'steen'){
-        result = 'De computer heeft gewonnen';
+        result = 'De computer heeft deze ronde gewonnen';
+        ++computerpunten;
     }
 
     if(computerChoice =='steen' && userChoice == 'schaar'){
-        result = 'De computer heeft gewonnen';
-        computerPoints++;
+        result = 'De computer heeft deze ronde gewonnen';
+        ++computerpunten;
     }else if(computerChoice =='schaar' && userChoice == 'steen'){
-        result = 'Jij hebt gewonnen';
+        result = 'Jij hebt deze ronde gewonnen';
+        ++gebruikerpunten;
     }
 
     if(computerChoice == 'papier' && userChoice == 'schaar'){
-        result ='Jij hebt gewonnen';
-        userPoints++;
+        result ='Jij hebt deze ronde gewonnen';
+        ++gebruikerpunten;
     }else if(computerChoice == 'schaar' && userChoice == 'papier'){
-        result ='De computer heeft gewonnen';
+        result ='De computer heeft deze ronde gewonnen';
+        ++computerpunten;
     }
   
     resultDisplay.innerHTML = result;
+    userPoints.innerHTML = gebruikerpunten;
+    computerPoints.innerHTML = computerpunten;
+   
+}
+
+function uiteindelijkeWinnaar() {
+    
+    if(gebruikerpunten > computerpunten && round === 5){
+        winnaar = "Gefeliciteerd,  jij hebt gewonnen";
+    }if(gebruikerpunten < computerpunten && round === 5){
+        winnaar = "De computer heeft gewonnen";
+    }else if(gebruikerpunten === computerpunten && round === 5){
+        winnaar = "Het is gelijkspel";
+    }
+    
+    
+    if(round === 5){
+        winner.innerHTML = winnaar;
+      
+       
+        
+    }
+
+
+ 
+
+
+}
+
+function reset() {
+    if(round === 5){
+    round = 0;
+    gebruikerpunten = 0;
+    computerpunten = 0;
+   
+    winner = "";
+
+    
+    }
 }
